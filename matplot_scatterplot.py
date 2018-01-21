38d42e0165acdf5a965166bcad5228c065ea1053
abonello <ab@anthonybonello.co.uk>
2018-01-21 20:39:21 +0000

    basic code for scatter plot

---
+++ matplot_scatterplot.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+# Scatter Plot - a simple type of graph.
+# Generally the idea of a scatter plot is to show
+# the correlation between two sets of data or
+# the distribution.
+# In 3D graphs you can see the correlation between 3 sets of data.
+
+
+
+
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('A title')
+# plt.legend()
+plt.show()
