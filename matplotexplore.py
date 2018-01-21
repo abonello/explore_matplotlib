0c4e86b82c8b6373787e585bedae073a0c283d7f
abonello <ab@anthonybonello.co.uk>
2018-01-21 19:28:23 +0000

    add a legend. Had to add labels to each line.

--- matplotexplore.py
+++ matplotexplore.py
@@ -12,9 +12,10 @@ y = [5,7,4,8]
 x2 = [1,2,3,4]
 y2 = [6,7,9,12]
 
-plt.plot(x, y)
-plt.plot(x2, y2)
+plt.plot(x, y, label = 'line 1')
+plt.plot(x2, y2, label = 'line 2')
 plt.xlabel('Plot Number')
 plt.ylabel('Important var')
 plt.title('Interesting graph')
+plt.legend()
 plt.show()
