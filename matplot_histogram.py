1ec9ed18acb930c90debd3cfd9ec4b9d1c181425
abonello <ab@anthonybonello.co.uk>
2018-01-21 20:19:10 +0000

    added list of population ages

--- matplot_histogram.py
+++ matplot_histogram.py
@@ -1,5 +1,11 @@
 import matplotlib.pyplot as plt
 
+# Histogram - show a distribution of some kind
+# useful when you have an array or a very long list
+# of things
+
+population_ages = [22,55,62,45,21,22,34,42,42,4,99,102,110,120,121,122,130,111,115,80,75,65,54,44,43,42,48]
+
 
 plt.xlabel('x')
 plt.ylabel('y')
