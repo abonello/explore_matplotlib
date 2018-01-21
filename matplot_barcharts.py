0eeb30a3d6a6fd83f6a411203b75d4ef7449e7b7
abonello <ab@anthonybonello.co.uk>
2018-01-21 20:10:03 +0000

    changed default color of bars

--- matplot_barcharts.py
+++ matplot_barcharts.py
@@ -10,8 +10,9 @@ y = [6,7,8,2,4]
 x2 = [1,3,5,9,11]
 y2 = [7,8,2,4,2]
 
-plt.bar(x, y, label = 'Bars 1')
-plt.bar(x2, y2, label = 'Bars 2')
+plt.bar(x, y, label = 'Bars 1', color = 'r')
+plt.bar(x2, y2, label = 'Bars 2', color = 'c')
+# There are various ways how to call colors, single letters, names, # numbers etc.
 
 plt.xlabel('x')
 plt.ylabel('y')
