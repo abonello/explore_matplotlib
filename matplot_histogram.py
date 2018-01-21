873eb1224bba9f70b032733eb41d18b023f80d93
abonello <ab@anthonybonello.co.uk>
2018-01-21 20:32:25 +0000

    code for histogram

--- matplot_histogram.py
+++ matplot_histogram.py
@@ -5,10 +5,17 @@ import matplotlib.pyplot as plt
 # of things
 
 population_ages = [22,55,62,45,21,22,34,42,42,4,99,102,110,120,121,122,130,111,115,80,75,65,54,44,43,42,48]
-ids = [x+1 for x in range(len(population_ages))]
+#ids = [x+1 for x in range(len(population_ages))]
+#plt.bar(ids, population_ages)
 
+# For histograms we do not need the id.
+# It has bins - a container for particular values
 
-plt.bar(ids, population_ages)
+bins = [0,10,20,30,40,50,60,70,80,90,100,110,120,130]
+# The number of bins will control the number of bars.
+# It can help to condense data.
+
+plt.hist(population_ages, bins, histtype='bar', rwidth=0.8)
 
 
 plt.xlabel('x')
