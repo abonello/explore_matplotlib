6ec81999ddcc36e5534e90983a20e5375b323bcf
abonello <ab@anthonybonello.co.uk>
2018-01-21 19:19:55 +0000

    add file

---
+++ matplotexplore.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+# plotting something
+# plt.plot(x, y)
+plt.plot([1,2,3], [5,7,4])
+plt.show()
+
+# data in variables, later can be non hard coded data filled by some function
+x = [1,2,3,4]
+y = [5,7,4,8]
+
+x2 = [1,2,3,4]
+y2 = [6,7,9,12]
+
+plt.plot(x, y)
+plt.plot(x2, y2)
+plt.xlabel('Plot Number')
+plt.ylabel('Important var')
+plt.title('Interesting graph')
+plt.show()
