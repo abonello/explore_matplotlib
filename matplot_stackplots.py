91d13871b780b1c7fb2e37e97228078d299df466
abonello <ab@anthonybonello.co.uk>
2018-01-22 12:51:33 +0000

    completed code for stack plot

--- matplot_stackplots.py
+++ matplot_stackplots.py
@@ -1,9 +1,20 @@
 import matplotlib.pyplot as plt
 
 # Explore Stack Plots
+# Usually used to convey a holistic number as well as its breakdown.
 
-plt.xlabel('x')
-plt.ylabel('y')
+# We will log 5 days of things happening.
+days = [1,2,3,4,5]
+
+sleeping = [7,8,6,9,7]
+eating   = [2,3,4,3,2]
+working  = [7,8,4,6,5]
+playing  = [8,5,10,6,10]
+
+plt.stackplot(days, sleeping, eating, working, playing, colors=['k', 'r', 'b', 'g'], labels=['sleeping', 'eating', 'working', 'playing'])
+
+plt.xlabel('days')
+plt.ylabel('hrs')
 plt.title("Stack Plots")
 plt.legend()
 plt.show()
