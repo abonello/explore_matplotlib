c129f067c03ad406d465ba35266ceb986bc10461
abonello <ab@anthonybonello.co.uk>
2018-01-22 14:14:56 +0000

    commented out the plot related code and added a few prints to see what is in the variables.

--- importing_data_from_file.py
+++ importing_data_from_file.py
@@ -3,8 +3,22 @@ import csv
 
 # We will import data from a file and plot it.
 
-plt.xlabel('')
-plt.ylabel('')
-plt.title("Data from File")
-plt.legend()
-plt.show()
+x = []
+y = []
+
+with open('example_data.csv', 'r') as csvfile:
+    plots = csv.reader(csvfile, delimiter=',')
+    print plots
+    for row in plots:
+        print row
+        x.append(int(row[0]))
+        y.append(int(row[1]))
+
+
+print x
+print y
+# plt.xlabel('')
+# plt.ylabel('')
+# plt.title("Data from File")
+# plt.legend()
+# plt.show()
