867c7cb3f69c97f778cba76d8f687f7f4a05831b
abonello <ab@anthonybonello.co.uk>
2018-01-22 14:47:38 +0000

    added plot

--- importing_data_from_file.py
+++ importing_data_from_file.py
@@ -8,17 +8,17 @@ y = []
 
 with open('example_data.csv', 'r') as csvfile:
     plots = csv.reader(csvfile, delimiter=',')
-    print plots
     for row in plots:
         print row
         x.append(int(row[0]))
         y.append(int(row[1]))
 
-
 print x
 print y
-# plt.xlabel('')
-# plt.ylabel('')
-# plt.title("Data from File")
-# plt.legend()
-# plt.show()
+
+plt.plot(x, y, label="loaded from file")
+plt.xlabel('')
+plt.ylabel('')
+plt.title("Data from File")
+plt.legend()
+plt.show()
