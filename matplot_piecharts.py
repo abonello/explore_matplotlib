b700ef6d47b6dadbea1278d85cd00d3d3850a6b2
abonello <ab@anthonybonello.co.uk>
2018-01-22 12:54:26 +0000

    prepare code for Pie Charts

---
+++ matplot_piecharts.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+# Pie Charts - Useful to show percentage taken by different categories.
+
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Pie Chart')
+plt.legend()
+plt.show()
